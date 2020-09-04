b981a323058a3d992f038f7d8d42e152ced9ab31
curtis <laurencechadwell@gmail.com>
2020-09-03 22:02:04 -0500

    Fixed omniauth issue

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -1,14 +1,20 @@
+require 'securerandom'
+
 class GithubController < ApplicationController
     skip_before_action :verified_user
   
     
     
     def create
+       
         @user = User.find_or_create_by(uid: auth['uid']) do |u|
-            u.username = auth['info']['username']
+            u.username = auth['info']['nickname']
+            u.password = SecureRandom.hex(10)
+            
+            
             
           end
-       
+          
           session[:user_id] = @user.id
        
           redirect_to @user
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_04_010715) do
-
-  create_table "authorizations", force: :cascade do |t|
-    t.string "provider"
-    t.string "uid"
-    t.integer "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2020_08_28_043700) do
 
   create_table "destinations", force: :cascade do |t|
     t.string "name"
@@ -59,13 +51,6 @@ ActiveRecord::Schema.define(version: 2020_09_04_010715) do
     t.datetime "updated_at", null: false
     t.string "provider"
     t.string "uid"
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
