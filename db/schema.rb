b4715913930a77ffac9fd0a2fa5ae97b69cd5d06
curtis <laurencechadwell@gmail.com>
2021-02-20 19:37:11 -0600

    Finshed making updates to rails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_28_043700) do
+ActiveRecord::Schema.define(version: 2021_02_21_005306) do
 
   create_table "destinations", force: :cascade do |t|
     t.string "name"
